3baa3d36f58bb529483d1cc24c4e40e084c5b5b4
Seeralan Sarvaharman <s.sarvaharman@bristol.ac.uk>
2023-07-06 15:26:10 +0100

    move extracting coords to its own function for increased generality

--- src/dynamo_rp/hull_generation.py
+++ src/dynamo_rp/hull_generation.py
@@ -59,26 +59,36 @@ def read_list_of_list_edges(file_name):
   return lines
 
 
-def get_hull_points_from_pdb(module_chain, pdb_fname, chain_label="a"):
+def get_hull_points_from_pdb(module_chain, pdb_fname, **kwargs):
     """
     Get the hull points from pdb file
     Parameters:
       module_chain: a list of modules present e.g. ['NcapD4', 'D4', 'D4', 'D4', 'CcapD4']
       pdb_fname: pdb filename
     Optional:
-      chain_label: label of chain in pdb file
+      **kwargs: keyword arguments for get_coords_calphas
+    """
+    starts_ends = np.concatenate(
+        rpt.get_helical_residue_location_for_chain(module_chain)
+    )
+
+    coords = get_coords_calphas(starts_ends.ravel(), pdb_fname, **kwargs)
+    starts = coords[0::2]
+    ends = coords[1::2]
+    return starts, ends
+
+def get_coords_calphas(idxs, pdb_fname, chain_label="a"):
+    """
+    Get the coordinates of C-alpha atoms from pdb file.
+    Parameters:
+        idxs: a list of residue indexes
+        pdb_fname: pdb filename
+    Optional:
+        chain_label: label of chain in pdb file
     """
     import Bio.PDB as bp
-
     parser = bp.PDBParser()
     structure = parser.get_structure("rp", pdb_fname)
-    starts_ends = np.concatenate(
-        ut.get_helical_residue_location_for_chain(module_chain)
-    )
     ar = np.array([res["CA"].coord for res in structure[0][chain_label]])
-    starts = []
-    ends = []
-    for i, j in starts_ends:
-        starts.append(ar[i - 1])
-        ends.append(ar[j - 1])
-    return np.array(starts), np.array(ends)
+    return ar[idxs]
+
